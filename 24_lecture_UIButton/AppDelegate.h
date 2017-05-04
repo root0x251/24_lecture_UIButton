275b2b3af68aee6c1f6315c4200deb8e06d9695d
Slava <rtootik@gmail.com>
2017-05-04 20:22:57 +0300

    Initial Commit

---
+++ 24_lecture_UIButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  24_lecture_UIButton
+//
+//  Created by Slava on 04.05.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 24_lecture_UIButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  24_lecture_UIButton
+//
+//  Created by Slava on 04.05.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
